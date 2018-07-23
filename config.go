6727903056827cb2b8cefa7ec377ef3dce78aad3
Ertai Duo <ertaiduo@uber.com>
2018-07-22 22:01:09 -0700

    Initial commit

---
+++ config.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os"
+	"encoding/json"
+)
+
+type Configuration struct {
+	Address      string
+	ReadTimeout  int64
+	WriteTimeout int64
+	Static       string
+}
+
+var config Configuration
+var logger *log.Logger
+
+func loadConfig() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalln("Cannot open config file", err)
+	}
+	defer func() {
+		err = file.Close()
+		if err != nil {
+			log.Fatal("Failed to close file", err)
+		}
+	}()
+
+	decoder := json.NewDecoder(file)
+	config = Configuration{}
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalln("Cannot get configuratin from file", err)
+	}
+}
+
+func init() {
+	loadConfig()
+	file, err := os.OpenFile("chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Fialed to open log file", err)
+	}
+	logger = log.New(file, "INFO ",  log.Ldate|log.Ltime|log.Lshortfile)
+}
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	// handle static resource
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	// index
+	mux.HandleFunc("/", )
+
+
+	// create a server
+	server := &http.Server{
+		Addr: config.Address,
+		Handler: mux,
+		ReadTimeout: time.Duration(config.ReadTimeout * int64(time.Second)),
+		WriteTimeout: time.Duration(config.WriteTimeout * int64(time.Second)),
+		MaxHeaderBytes: 1 << 20,
+	}
+	server.ListenAndServe()
+}
